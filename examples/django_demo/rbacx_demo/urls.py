8c06d5b6891870eb6a8a7370c1d13e3bfa3a9cba
Cheater121 <cheater1211@gmail.com>
2025-09-17 20:01:37 +0300

    feat: add documentation for running demo apps and HTTP decision mappings

--- examples/django_demo/rbacx_demo/urls.py
+++ examples/django_demo/rbacx_demo/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("health", views.health, name="health"),
-    path("docs", views.docs, name="docs"),
+    path("doc", views.doc, name="doc"),
     path("admin/", admin.site.urls),
 ]
